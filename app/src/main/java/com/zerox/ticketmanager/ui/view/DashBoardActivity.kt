4338947dd08f7bb41108f536b40f905aea42db54
Abel <abochoa91@gmail.com>
2022-10-08 01:47:42 -0400

    create RecyclerView and function buttons

--- app/src/main/java/com/zerox/ticketmanager/ui/view/DashBoardActivity.kt
+++ app/src/main/java/com/zerox/ticketmanager/ui/view/DashBoardActivity.kt
@@ -1,14 +1,33 @@
 package com.zerox.ticketmanager.ui.view
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.zerox.ticketmanager.databinding.ActivityDashBoardBinding
+import androidx.appcompat.app.AppCompatActivity
+import com.zerox.ticketmanager.databinding.ActivityDashboardBinding
+import com.zerox.ticketmanager.ui.utils.ViewAnimation
 
 class DashBoardActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityDashBoardBinding
+    private lateinit var binding: ActivityDashboardBinding
+    private var isRotate = false
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityDashBoardBinding.inflate(layoutInflater)
+        binding = ActivityDashboardBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        // initialization for the menu elements
+        ViewAnimation.init(binding.fabWorkTicket)
+        ViewAnimation.init(binding.fabGetDirections)
+
+        //on click to display menu elements with animation
+        binding.fabMenu.setOnClickListener {
+            isRotate = ViewAnimation.rotateFab(it, !isRotate)
+            if(isRotate){
+                ViewAnimation.showIn(binding.fabWorkTicket)
+                ViewAnimation.showIn(binding.fabGetDirections)
+            }else{
+                ViewAnimation.showOut(binding.fabWorkTicket)
+                ViewAnimation.showOut(binding.fabGetDirections)
+            }
+        }
+
     }
 }
