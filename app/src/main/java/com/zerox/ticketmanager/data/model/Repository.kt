4f2082876edf243b314cc5d93dd655930e6d81e4
Abel <abochoa91@gmail.com>
2022-10-07 21:50:32 -0400

    add exception handling for user

--- app/src/main/java/com/zerox/ticketmanager/data/model/Repository.kt
+++ app/src/main/java/com/zerox/ticketmanager/data/model/Repository.kt
@@ -2,16 +2,23 @@ package com.zerox.ticketmanager.data.model
 
 import com.zerox.ticketmanager.data.model.database.dao.UserDao
 import com.zerox.ticketmanager.data.model.database.entities.UserEntitiy
+import com.zerox.ticketmanager.data.model.exceptions.UserNotFoundException
 import javax.inject.Inject
 
 class Repository @Inject constructor(private val userDao: UserDao) {
 
     suspend fun getUserById(id:Int):UserEntitiy{
-        return userDao.getUserById(id)
+        val user = userDao.getUserById(id)
+        if (user == null)
+            throw UserNotFoundException("There is no user with the provided id")
+        return user
     }
 
     suspend fun getUserByUsername(username:String):UserEntitiy{
-        return userDao.getUserByUsername(username)
+        val user = userDao.getUserByUsername(username)
+        if (user == null)
+            throw UserNotFoundException("There is no user with the provided username")
+        return user
     }
 
     suspend fun addUser(user:UserEntitiy):Int{
---
+++ app/src/main/java/com/zerox/ticketmanager/data/model/exceptions/UserNotFoundException.kt
@@ -0,0 +1,4 @@
+package com.zerox.ticketmanager.data.model.exceptions
+
+class UserNotFoundException(cause:String) : Exception(cause) {
+}
