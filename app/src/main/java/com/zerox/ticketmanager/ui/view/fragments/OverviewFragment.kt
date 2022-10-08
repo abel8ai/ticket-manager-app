a6baf4b4a6981746dfbf55e7538ba250ce4a2c10
Abel <abochoa91@gmail.com>
2022-10-08 16:55:55 -0400

    create Overview UI

--- app/src/main/java/com/zerox/ticketmanager/ui/view/fragments/OverviewFragment.kt
+++ app/src/main/java/com/zerox/ticketmanager/ui/view/fragments/OverviewFragment.kt
@@ -5,10 +5,17 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
 import com.zerox.ticketmanager.R
 import com.zerox.ticketmanager.databinding.FragmentOverviewBinding
+import com.zerox.ticketmanager.ui.viewmodel.WorkTicketViewModel
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class OverviewFragment : Fragment() {
+
+    //viewModel Injection
+    private val workTicketViewModel by viewModels<WorkTicketViewModel> ()
     private var _binding: FragmentOverviewBinding? = null
     private val binding get() = _binding!!
     private var ticketId = -1
