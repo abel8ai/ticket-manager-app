e0ed92a808e3835fed33274b002615d1d6b97829
Abel <abochoa91@gmail.com>
2022-10-07 22:04:22 -0400

    create the login form in LoginActivity

--- app/src/main/java/com/zerox/ticketmanager/LoginActivity.kt
+++ app/src/main/java/com/zerox/ticketmanager/LoginActivity.kt
@@ -2,10 +2,21 @@ package com.zerox.ticketmanager
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
+import com.zerox.ticketmanager.databinding.ActivityLoginBinding
+import com.zerox.ticketmanager.ui.viewmodel.LoginViewModel
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class LoginActivity : AppCompatActivity() {
+    private val loginViewModel : LoginViewModel by viewModels()
+    private lateinit var binding : ActivityLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.btnLogin.setOnClickListener {
+            
+        }
     }
 }
