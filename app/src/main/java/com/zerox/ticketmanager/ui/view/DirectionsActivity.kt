4173879e92b3ef4865911e719e716489de0d46ad
Abel <abochoa91@gmail.com>
2022-10-10 02:04:26 -0400

    correct ui and functions for Directions screen

--- app/src/main/java/com/zerox/ticketmanager/ui/view/DirectionsActivity.kt
+++ app/src/main/java/com/zerox/ticketmanager/ui/view/DirectionsActivity.kt
@@ -6,7 +6,6 @@ import android.content.pm.PackageManager
 import android.location.Location
 import android.os.Bundle
 import android.util.Log
-import android.view.View
 import android.widget.SearchView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
@@ -27,6 +26,7 @@ import com.google.android.libraries.places.api.net.PlacesClient
 import com.google.maps.DirectionsApi
 import com.google.maps.GeoApiContext
 import com.google.maps.android.PolyUtil
+import com.google.maps.errors.NotFoundException
 import com.google.maps.model.DirectionsResult
 import com.google.maps.model.TravelMode
 import com.zerox.ticketmanager.BuildConfig
@@ -69,7 +69,7 @@ class DirectionsActivity : AppCompatActivity(), OnMapReadyCallback {
         destination = intent.extras?.getString("direction")
 
         // set the direction in searchview if it comes from work ticket screen
-        if(destination != null){
+        if (destination != null) {
             binding.svDirection.setQuery(destination, false)
         }
 
@@ -80,17 +80,28 @@ class DirectionsActivity : AppCompatActivity(), OnMapReadyCallback {
                 if (query != null) {
                     destination = query
                     CoroutineScope(Dispatchers.IO).launch {
-                        val result = getRoute()
-                        runOnUiThread {
-                            // adds gps markers in the map
-                            addMarkersToMap(result,map!!)
-                            // draws the route line
-                            addPolyline(result,map!!)
+                        try {
+                            val result = getRoute()
+                            runOnUiThread {
+                                // adds gps markers in the map
+                                addMarkersToMap(result, map!!)
+                                // draws the route line
+                                addPolyline(result, map!!)
+                            }
+                        } catch (exception: NotFoundException) {
+                            runOnUiThread {
+                                Toast.makeText(
+                                    this@DirectionsActivity,
+                                    "Unable to find destination",
+                                    Toast.LENGTH_SHORT
+                                ).show()
+                            }
                         }
                     }
                 }
                 return false
             }
+
             override fun onQueryTextChange(newText: String?): Boolean {
                 return false
             }
@@ -126,17 +137,11 @@ class DirectionsActivity : AppCompatActivity(), OnMapReadyCallback {
         onBackPressed()
         return super.onSupportNavigateUp()
     }
+
     override fun onMapReady(map: GoogleMap) {
         this.map = map
         // Prompt the user for permission.
         getLocationPermission()
-
-        // Turn on the My Location layer and the related control on the map.
-        updateLocationUI()
-
-        // Get the current location of the device and set the position of the map.
-        getDeviceLocation()
-        // setUserLocation
     }
 
     private fun getLocationPermission() {
@@ -152,6 +157,11 @@ class DirectionsActivity : AppCompatActivity(), OnMapReadyCallback {
             == PackageManager.PERMISSION_GRANTED
         ) {
             locationPermissionGranted = true
+            // Turn on the My Location layer and the related control on the map.
+            updateLocationUI()
+
+            // Get the current location of the device and set the position of the map.
+            getDeviceLocation()
         } else {
             ActivityCompat.requestPermissions(
                 this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
@@ -183,17 +193,26 @@ class DirectionsActivity : AppCompatActivity(), OnMapReadyCallback {
                                 )
                             )
                             // get directions to destination from Directions API
-                            if (destination!=null)
+                            if (destination != null)
                                 CoroutineScope(Dispatchers.IO).launch {
-                                    val result = getRoute()
-                                    runOnUiThread {
-                                        // adds gps markers in the map
-                                        addMarkersToMap(result,map!!)
-                                        // draws the route line
-                                        addPolyline(result,map!!)
+                                    try {
+                                        val result = getRoute()
+                                        runOnUiThread {
+                                            // adds gps markers in the map
+                                            addMarkersToMap(result, map!!)
+                                            // draws the route line
+                                            addPolyline(result, map!!)
+                                        }
+                                    } catch (exception: Exception) {
+                                        runOnUiThread {
+                                            Toast.makeText(
+                                                this@DirectionsActivity,
+                                                exception.message,
+                                                Toast.LENGTH_SHORT
+                                            ).show()
+                                        }
                                     }
                                 }
-
                         }
                     } else {
                         Log.d(TAG, "Current location is null. Using defaults.")
@@ -225,11 +244,16 @@ class DirectionsActivity : AppCompatActivity(), OnMapReadyCallback {
                     grantResults[0] == PackageManager.PERMISSION_GRANTED
                 ) {
                     locationPermissionGranted = true
+                    // Turn on the My Location layer and the related control on the map.
+                    updateLocationUI()
+
+                    // Get the current location of the device and set the position of the map.
+                    getDeviceLocation()
+                    // setUserLocation
                 }
             }
             else -> super.onRequestPermissionsResult(requestCode, permissions, grantResults)
         }
-        updateLocationUI()
     }
 
     /**
@@ -254,6 +278,7 @@ class DirectionsActivity : AppCompatActivity(), OnMapReadyCallback {
             Log.e("Exception: %s", e.message, e)
         }
     }
+
     // establishes the conecction with the Directons API
     private fun getGeoContext(): GeoApiContext? {
         val geoApiContext = GeoApiContext()
@@ -263,26 +288,20 @@ class DirectionsActivity : AppCompatActivity(), OnMapReadyCallback {
             .setReadTimeout(1, TimeUnit.SECONDS)
             .setWriteTimeout(1, TimeUnit.SECONDS)
     }
+
     private fun getRoute(): DirectionsResult {
         val now = DateTime()
         val lat = lastKnownLocation!!.latitude.toString()
         val long = lastKnownLocation!!.longitude.toString()
         val origin = "$lat,$long"
-        return DirectionsApi.newRequest(getGeoContext()).mode(TravelMode.DRIVING).origin(origin)
-            .destination(destination)
-            .departureTime(now).await()
+        val route =
+            DirectionsApi.newRequest(getGeoContext()).mode(TravelMode.DRIVING).origin(origin)
+                .destination(destination).departureTime(now)
+        return route.await()
     }
+
     private fun addMarkersToMap(results: DirectionsResult, mMap: GoogleMap) {
-        mMap.addMarker(
-            MarkerOptions().position(
-                LatLng(
-                    results.routes[0].legs[0].startLocation.lat,
-                    results.routes[0].legs[0].startLocation.lng
-                )
-            ).title(
-                results.routes[0].legs[0].startAddress
-            )
-        )
+        mMap.clear()
         mMap.addMarker(
             MarkerOptions().position(
                 LatLng(
@@ -294,12 +313,14 @@ class DirectionsActivity : AppCompatActivity(), OnMapReadyCallback {
             )
         )
     }
+
     private fun addPolyline(results: DirectionsResult, mMap: GoogleMap) {
         val decodedPath: List<LatLng> = PolyUtil.decode(
             results.routes[0].overviewPolyline.encodedPath
         )
         mMap.addPolyline(PolylineOptions().addAll(decodedPath))
     }
+
     companion object {
         private val TAG = DirectionsActivity::class.java.simpleName
         private const val DEFAULT_ZOOM = 15
